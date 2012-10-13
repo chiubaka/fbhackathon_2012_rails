1f5cd15c990b62c04040b817dca37f66b556fbda
Daniel Chiu <dwchiu@stanford.edu>
2012-10-12 22:19:44 -0700

    Fix JSON.

--- app/controllers/from_links_controller.rb
+++ app/controllers/from_links_controller.rb
@@ -2,15 +2,18 @@ class FromLinksController < ApplicationController
   def link
     @from_link = FromLink.find_or_create_by_url(params[:from_link][:url])
     if @from_link.save
-      to_link = ToLink.find_or_create_by_url_and_form_link_id(url: params[:from_link][:to_link], from_link_id: @from_link.id)
+      to_link = ToLink.find_by_from_link_id_and_url(@from_link.id, params[:from_link][:to_link]) 
+      to_link = ToLink.new(url: params[:from_link][:to_link], from_link_id: @from_link.id) if to_link.nil?
+      
       to_link.frequency += 1
       if to_link.save
         next_link = FromLink.find_by_url(params[:from_link][:to_link])
-        render json: next_link
+        render json: next_link.to_json(include: [:to_links])
       else
         render json: to_link.errors, status: :unprocessable_entity
       end
     else
+      logger.debug "Returning error JSON."
       render json: @from_link.errors, status: :unprocessable_entity
     end
   end
