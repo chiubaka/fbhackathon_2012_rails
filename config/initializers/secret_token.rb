b1e1c57826e7b84f8c66cee2881f52dd0d7e0a64
Daniel Chiu <dwchiu@stanford.edu>
2012-10-12 19:53:51 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbhackathon2012Rails::Application.config.secret_token = '5f0e14a151efaa8fb732409b5fb748f8c5313f6847829d27104128e1eaa2e925466c072c4014d29f3361ebd7be343da2f1e2b3c032962aae67721c243fad4bbb'
